b6309cc05ad3d53afafd8c09e2989a3ff508e1fc
Parul Gupta <guptaparul1002@gmail.com>
2021-12-07 20:05:09 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 var checkButton = document.querySelector("#check button");
 var cashGiven = document.querySelector("#cash given");
 var billAmount = document.querySelector("#bill amount");
-var noOfNotes = document.querySelector(".no of notes");
+var noOfNotes = document.querySelectorAll(".no of notes");
 const message = document.querySelector("#error-message");
 
 var availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
 
-checkButton.addEventListener("click", clickEventHandler);
+function calculateChange(amountToBeReturned) {
+      
+  for (let i = 0; i < availableNotes.length; i++) {
+    
+    var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    
+
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+
 function clickEventHandler() {
     if (billAmount.value > 0) {
         
@@ -23,17 +34,7 @@ function clickEventHandler() {
       }
     };
     
-    function calculateChange(amountToBeReturned) {
-      
-      for (let i = 0; i < availableNotes.length; i++) {
-        
-        var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
-        
-    
-        noOfNotes[i].innerText = numberOfNotes;
-      }
+
     }
     
     function hideMessage() {
@@ -46,4 +47,4 @@ function clickEventHandler() {
     }
     
 
-
+    checkButton.addEventListener("click", clickEventHandler);

eb982fd34d3a5ef1d06701bfac5ffa868f456303
Parul Gupta <guptaparul1002@gmail.com>
2021-12-07 22:30:43 +0530

    final commit

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
-
-var checkButton = document.querySelector("#check button");
-var cashGiven = document.querySelector("#cash given");
-var billAmount = document.querySelector("#bill amount");
-var noOfNotes = document.querySelectorAll(".no of notes");
+var checkButton = document.querySelector("#check-button");
+var cashGiven = document.querySelector("#cash-given");
+var billAmount = document.querySelector("#bill-amount");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 const message = document.querySelector("#error-message");
 
-var availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
-
+var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 function calculateChange(amountToBeReturned) {
-      
+hideMessage();
   for (let i = 0; i < availableNotes.length; i++) {
-    
     var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-
     amountToBeReturned = amountToBeReturned % availableNotes[i];
-    
-
     noOfNotes[i].innerText = numberOfNotes;
   }
+}
 
-function clickEventHandler() {
-    if (billAmount.value > 0) {
-        
-        if (cashGiven.value >= billAmount.value) {
-            var amountToBeReturned = cashGiven.value - billAmount.value; 
-            calculateChange(amountToBeReturned);
-          } else {
-            showMessage("Cash provided should atleast  be equal to the bill amount");
-        }
-      } else {
-        showMessage("Invalid Bill Amount");
-      }
-    };
-    
-
+function clickEventHandler(){
+  if (Number(billAmount.value) > 0) {
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      var amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value); 
+      calculateChange(amountToBeReturned);
+    } 
+    else {
+      showMessage("Cash provided should atleast be equal to the bill amount");
     }
+  }
+  else {
+    showMessage("Invalid Bill Amount");
+  }
+}
     
     function hideMessage() {
       message.style.display = "none";
@@ -46,5 +39,5 @@ function clickEventHandler() {
       message.innerText = msg;
     }
     
-
-    checkButton.addEventListener("click", clickEventHandler);
+    
+checkButton.addEventListener("click", clickEventHandler);    
